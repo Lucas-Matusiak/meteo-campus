4ed8182cb764d817dbdf6f484d8f9bc73aabb5d9
Lucas-Matusiak <lucassmatusiak@gmail.com>
2024-03-29 16:23:37 +0100

    ajout composant barre de recherche vide

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">Hello world !</h1>
+    <BarreDeRecherche />
   </div>
 </template>
+<script>
+import BarreDeRecherche from '~/components/BarreDeRecherche.vue'
+
+export default {
+  components: {
+    BarreDeRecherche
+  }
+  // Autres options du composant parent
+}
+</script>
---
+++ components/BarreDeRecherche.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- Contenu de votre sous-composant -->
+    <input type="search" id="site-search" name="q" />
+  </div>
+</template>
+
+<script>
+export default {
+  // Options du composant
+};
+</script>
+
+<style scoped>
+input {
+  margin: 0.4rem 0;
+  border: 1px solid
+}
+
+</style>
