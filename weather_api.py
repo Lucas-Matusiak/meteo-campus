a342ea0d22cc23f1db236d3b23ad0b28b38cb572
Bambouzle <leherosdu02@hotmail.fr>
2024-04-02 16:24:24 +0200

    weather api hourly

--- weather_api.py
+++ weather_api.py
@@ -41,7 +41,39 @@ def get_current_weather(lat, lon, api_key):
         raise Exception(f"Non-success status code: {response.status_code}")
 
 def get_hourly_forecast(lat, lon, api_key):
-    response = requests.get("https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={API key}")
-    return
+    response = requests.get(f"https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={api_key}&units=metric&mode=xml&cnt=5")
 
-get_current_weather(lat, lon, api_key)
+    xml_content = response.text.replace('<?xml version="1.0" encoding="UTF-8"?>\n', '')
+    root = ET.fromstring(xml_content)
+
+    # Extract elements
+    location_name = root.find('.//name').text
+    country = root.find('.//country').text
+    forecast_times = root.findall('.//forecast/time')
+
+    # Create a list of dictionaries to store data for each hour
+    forecast_data = []
+
+    # Extract data for each hour and store in the list of dictionaries
+    for time in forecast_times:
+        data = {
+            'from_time': time.get('from'),
+            'to_time': time.get('to'),
+            'symbol': time.find('.//symbol').get('name'),
+            'precipitation': float(time.find('.//precipitation').get('probability')),
+            'wind_direction': time.find('.//windDirection').get('name'),
+            'wind_speed': time.find('.//windSpeed').get('name'),
+            'temperature': round(float(time.find('.//temperature').get('value'))),
+            'feels_like': round(float(time.find('.//feels_like').get('value'))),
+            'pressure': float(time.find('.//pressure').get('value')),
+            'humidity': round(float(time.find('.//humidity').get('value'))),
+            'clouds': time.find('.//clouds').get('value')
+        }
+        forecast_data.append(data)
+
+    # Print the list of dictionaries for demonstration
+    for data in forecast_data:
+        print(data['from_time'])
+        print(data)
+
+get_hourly_forecast(lat, lon, api_key)
