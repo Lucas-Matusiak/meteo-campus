2b080e0bef0194ac309b357589d6df73d0512370
Lucas-Matusiak <lucassmatusiak@gmail.com>
2024-04-02 16:28:43 +0200

    ajout barre de recherche affichage mobile et web avec data en dur

--- app.vue
+++ app.vue
@@ -1,19 +1,17 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">Choisi ton campus</h1>
+  <div class="m-8 flex justify-center items-center flex-col">
+    <h1 class="m-5 text-2xl font-bold">Choisis ton campus</h1>
     <BarreDeRecherche />
   </div>
 </template>
 <script>
-import BarreDeRecherche from '~/components/BarreDeRecherche.vue'
+import BarreDeRecherche from "~/components/BarreDeRecherche.vue";
 
 export default {
   components: {
-    BarreDeRecherche
-  }
+    BarreDeRecherche,
+  },
   // Autres options du composant parent
-}
+};
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
--- components/BarreDeRecherche.vue
+++ components/BarreDeRecherche.vue
@@ -1,23 +1,26 @@
 <template>
-  <div>
+  <div class="m-5">
     <input
       type="text"
       v-model="input"
       placeholder="Recherchez un campus..."
       class="block w-64 mt-6 px-4 py-2 rounded bg-white bg-no-repeat bg-right border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
     />
-    <div v-if="filteredList().length > 0" class="grid grid-cols-1 gap-4 mt-4">
-      <div
-        v-for="campus in filteredList()"
-        :key="campus"
-        class="bg-blue-500 text-white rounded p-4"
-      >
-        <p @click="updateInput(campus)">{{ campus }}</p>
-        <!-- Ajout du gestionnaire d'événements @click -->
+    <div v-if="input">
+      <div v-if="filteredList().length" class="grid grid-cols-1 gap-4 mt-4">
+        <div
+          v-for="campus in filteredList()"
+          :key="campus"
+          class="bg-blue-500 text-white rounded p-4"
+          @click="updateInput(campus)"
+        >
+          <p>{{ campus }}</p>
+          <!-- Ajout du gestionnaire d'événements @click -->
+        </div>
       </div>
-    </div>
-    <div v-else class="bg-red-500 text-white rounded p-4 mt-4">
-      <p>No results found!</p>
+      <div v-else class="bg-red-500 text-white rounded p-4 mt-4">
+        <p>Aucun résultat trouvé</p>
+      </div>
     </div>
   </div>
 </template>
@@ -25,15 +28,26 @@
 <script setup>
 import { ref } from "vue";
 let input = ref("");
-const fruits = ["apple", "banana", "orange"];
+let display = false;
+const fruits = [
+  "apple",
+  "banana",
+  "orange",
+  "orange",
+  "orange",
+  "orange",
+  "orange",
+  "orange",
+];
 function filteredList() {
-  return fruits.filter((fruit) =>
-    fruit.toLowerCase().includes(input.value.toLowerCase())
-  );
+  return fruits
+    .filter((fruit) => fruit.toLowerCase().includes(input.value.toLowerCase()))
+    .slice(0, 5);
 }
 
 function updateInput(value) {
   input.value = value; // Met à jour la valeur de l'input avec celle du campus
+  this.display.false;
 }
 </script>
 
