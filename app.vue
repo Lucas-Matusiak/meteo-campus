851b8f9894ad819c17a2887be752d436195814fa
Lucas-Matusiak <lucassmatusiak@gmail.com>
2024-04-02 14:50:43 +0200

    ajout bar de recherche exemple

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">Hello world !</h1>
+    <h1 class="text-3xl font-bold underline">Choisi ton campus</h1>
+    <BarreDeRecherche />
   </div>
 </template>
+<script>
+import BarreDeRecherche from '~/components/BarreDeRecherche.vue'
+
+export default {
+  components: {
+    BarreDeRecherche
+  }
+  // Autres options du composant parent
+}
+</script>
+<style scoped>
+
+</style>
