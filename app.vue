b8f6ee36a9e37b1395318537efc171604550292b
Adama20 <65755121+Adama20@users.noreply.github.com>
2024-04-03 11:50:27 +0200

    footer

--- app.vue
+++ app.vue
@@ -3,6 +3,17 @@
     <h1 class="text-3xl font-bold underline">Hello world !</h1>
     <BarreDeRecherche />
   </div>
+
+  <footer class="footer">
+    <div class="container">
+      <div class="footer-buttons">
+        <button class="button">Nous contacter</button>
+        <button class="button">Mentions légales</button>
+        <button class="button">Nos réseaux</button>
+      </div>
+    </div>
+  </footer>
+
 </template>
 <script>
 import BarreDeRecherche from '~/components/BarreDeRecherche.vue'
@@ -14,3 +25,27 @@ export default {
   // Autres options du composant parent
 }
 </script>
+
+<style>
+.footer {
+  background-color: #ffffff; /* Couleur blanche */
+  padding: 20px 0; /* Espacement intérieur */
+  text-align: center;
+}
+
+.footer-buttons {
+  display: flex;
+  justify-content: center;
+}
+
+.button {
+  background-color: #ffffff; /* Couleur blanche */
+  color: #000000; /* Couleur noire */
+  border: none;
+  padding: 10px 20px;
+  margin: 0 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  outline: none;
+}
+</style>
