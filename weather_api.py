8cb7ed86ff115e26bc4196d16c5e6a67fc4549e0
Bambouzle <leherosdu02@hotmail.fr>
2024-04-03 14:51:44 +0200

    création de l'api pour le front et modification des données retournées dans les 3 fonctions

--- weather_api.py
+++ weather_api.py
@@ -3,77 +3,129 @@ import requests
 from lxml import etree as ET
 from datetime import datetime
 
-lat = '44.34'
-lon = '10.99'
-api_key = 'a1b1045de421855d4d44bb2b53d4da8f'
 def get_current_weather(lat, lon, api_key):
-    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric&mode=xml&lang=fr")
+    api_call = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric&mode=xml&lang=fr"
+    response = requests.get(api_call)
     if response:
-        # print(response.text)
-
         xml_content = response.text.replace('<?xml version="1.0" encoding="UTF-8"?>\n', '')
         root = ET.fromstring(str(xml_content))
-        # print(xml_content)
 
-        city_name = root.find('city').attrib['name']
-        sun_rise = root.find('city').find('sun').attrib['rise']
-        print(sun_rise)
+        # Accessing elements
         temperature_value = round(float(root.find('temperature').attrib['value']))
-        temperature_unit = root.find('temperature').attrib['unit']
+        feels_like_value =round(float(root.find('feels_like').attrib['value']))
         humidity_value = root.find('humidity').attrib['value']
-        humidity_unit = root.find('humidity').attrib['unit']
-        pressure_value = root.find('pressure').attrib['value']
-        pressure_unit = root.find('pressure').attrib['unit']
+        
+        weather_description = root.find('weather').attrib['value']
         wind_speed = round(3.6 * float(root.find('wind/speed').attrib['value']))
-        wind_speed_unit = root.find('wind/speed').attrib['unit']
-        wind_speed_name = root.find('wind/speed').attrib['name']
-        wind_direction_value = root.find('wind/direction').attrib['value']
-        wind_direction_code = root.find('wind/direction').attrib['code']
-        wind_direction_name = root.find('wind/direction').attrib['name']
-        clouds_value = root.find('clouds').attrib['value']
-        clouds_name = root.find('clouds').attrib['name']
-        visibility_value = root.find('visibility').attrib['value']
-        last_update_value = root.find('lastupdate').attrib['value']
+        sun_rise_datetime = datetime.strptime(root.find('city/sun').attrib['rise'], '%Y-%m-%dT%H:%M:%S')
+        sun_set_datetime = datetime.strptime(root.find('city/sun').attrib['set'], '%Y-%m-%dT%H:%M:%S')
+        sun_rise = str(sun_rise_datetime.strftime('%H:%M'))
+        sun_set = str(sun_set_datetime.strftime('%H:%M'))
 
-        # return {city_name, temperature_value, temperature_unit, humidity_value, humidity_unit, pressure_value, pressure_unit,
-        #     wind_speed, }
+        # clouds_name = root.find('clouds').attrib['name']
+        # clouds_value = root.find('clouds').attrib['value']
+        # temperature_unit = "°c"#root.find('temperature').attrib['unit']
+        # humidity_unit = root.find('humidity').attrib['unit']
+        # feels_like_unit = "°c"
+        # wind_speed_unit = "km/h"
+        # city_name = root.find('city').attrib['name']
+        # last_update_value = root.find('lastupdate').attrib['value']
+        # wind_speed_name = root.find('wind/speed').attrib['name']
+        # wind_direction_value = root.find('wind/direction').attrib['value']
+        # wind_direction_code = root.find('wind/direction').attrib['code']
+        # wind_direction_name = root.find('wind/direction').attrib['name']
+        # pressure_value = root.find('pressure').attrib['value']
+        # pressure_unit = root.find('pressure').attrib['unit']
+        # visibility_value = root.find('visibility').attrib['value']
+        # clouds_unit = "%"
+
+
+        return {
+            "temperature": temperature_value,
+            "feels_like_value": feels_like_value,
+            "humidity": humidity_value,
+            "weather_description": weather_description,
+            "wind_speed": wind_speed,
+            "sun_rise": sun_rise,
+            "sun_set": sun_set
+        }
+
     else:
         raise Exception(f"Non-success status code: {response.status_code}")
 
+
 def get_hourly_forecast(lat, lon, api_key):
-    response = requests.get(f"https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={api_key}&units=metric&mode=xml&cnt=5")
+    api_call = f"https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={api_key}&units=metric&mode=xml&cnt={5}&lang=fr"
+    response = requests.get(api_call)
+    
+    if response:
+        xml_content = response.text.replace('<?xml version="1.0" encoding="UTF-8"?>\n', '')
+        root = ET.fromstring(xml_content)
 
-    xml_content = response.text.replace('<?xml version="1.0" encoding="UTF-8"?>\n', '')
-    root = ET.fromstring(xml_content)
+        # Extract elements
+        forecast_times = root.findall('.//forecast/time')
+        forecast_data = []
 
-    # Extract elements
-    location_name = root.find('.//name').text
-    country = root.find('.//country').text
-    forecast_times = root.findall('.//forecast/time')
+        # Extract data for each hour and store in a list of dictionaries
+        for hour in forecast_times:
+            date = datetime.strptime(hour.get('from'), '%Y-%m-%dT%H:%M:%S')
+            time =  str(date.strftime('%H:%M'))
 
-    # Create a list of dictionaries to store data for each hour
-    forecast_data = []
+            wind_speed_mps = float(hour.find('.//windSpeed').get('mps'))
+            wind_speed_kph = round(wind_speed_mps * 3.6)
 
-    # Extract data for each hour and store in the list of dictionaries
-    for time in forecast_times:
-        data = {
-            'from_time': time.get('from'),
-            'to_time': time.get('to'),
-            'symbol': time.find('.//symbol').get('name'),
-            'precipitation': float(time.find('.//precipitation').get('probability')),
-            'wind_direction': time.find('.//windDirection').get('name'),
-            'wind_speed': time.find('.//windSpeed').get('name'),
-            'temperature': round(float(time.find('.//temperature').get('value'))),
-            'feels_like': round(float(time.find('.//feels_like').get('value'))),
-            'pressure': float(time.find('.//pressure').get('value')),
-            'humidity': round(float(time.find('.//humidity').get('value'))),
-            'clouds': time.find('.//clouds').get('value')
-        }
-        forecast_data.append(data)
+            weather_description = hour.find('.//symbol').get('name')
+            precipitation_proba = float(hour.find('.//precipitation').get('probability'))
+            temperature = round(float(hour.find('.//temperature').get('value')))
+            
+            # wind_direction = hour.find('.//windDirection').get('name')
+            # feels_like = round(float(hour.find('.//feels_like').get('value')))
+            # pressure = float(hour.find('.//pressure').get('value'))
+            # humidity = round(float(hour.find('.//humidity').get('value')))            
 
-    # Print the list of dictionaries for demonstration
-    for data in forecast_data:
-        print(data['from_time'])
-        print(data)
+            data = {
+                'time': time,
+                'weather_description': weather_description,
+                'precipitation_proba': precipitation_proba,
+                'wind_speed': wind_speed_kph,
+                'temperature': temperature,
+            }
+            forecast_data.append(data)
 
-get_hourly_forecast(lat, lon, api_key)
+        return forecast_data
+    else:
+        raise Exception(f"Non-success status code: {response.status_code}")
+
+def get_daily_forecast(lat,lon,api_key):
+    api_call = f"https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={7}&appid={api_key}&mode=xml&units=metric&lang=fr"
+    response = requests.get(api_call)
+    if response:
+        xml_content = response.text.replace('<?xml version="1.0" encoding="UTF-8"?>\n', '')
+        root = ET.fromstring(str(xml_content))
+
+        forecast_data = []
+        forecast_times = root.findall(".//time")
+
+        for day in forecast_times:
+            date = day.get("day")
+
+            temperature_day = round(float(day.find("temperature").get("day")))
+            temperature_max = round(float(day.find("temperature").get("max")))
+            temperature_min = round(float(day.find("temperature").get("min")))
+            wind_speed_mps = float(day.find("windSpeed").get("mps"))
+            wind_speed_kmh = round(wind_speed_mps * 3.6)
+            weather_description = day.find("symbol").get("name")
+
+            day_data = {
+                "date": date,
+                "weather_description": weather_description,
+                "temperature_day": temperature_day,
+                "temperature_max": temperature_max,
+                "temperature_min": temperature_min,
+                "wind_speed_kmh": wind_speed_kmh
+            }
+            forecast_data.append(day_data)
+
+        return forecast_data
+    else:
+        raise Exception(f"Non-success status code: {response.status_code}")
