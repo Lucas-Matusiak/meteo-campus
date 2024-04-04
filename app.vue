8291d95febabf7660336b96738d16ad5f974fd73
Lucas-Matusiak <49200401+Lucas-Matusiak@users.noreply.github.com>
2024-04-04 11:33:10 +0200

    espacement des blocks page d'accueil

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex justify-center items-center flex-col mx-10">
+  <div class="flex justify-center items-center flex-col mt-10">
     <img
       src="~/../assets/images/logo.png"
       alt="Description de l'image"
-      class="w-20 h-auto mx-4"
+      class="w-20 h-auto m-4 "
     />
     <NuxtPage />
 
-    <div class="flex items-center justify-center bg-white py-4">
+    <div class="flex items-center justify-center bg-white py-4 mt-10">
       <div class="flex space-x-4">
         <button class="button">Nous contacter</button>
         <button class="button">Mentions légales</button>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="m-8 flex justify-center items-center flex-col">
+  <div class="flex justify-center items-center flex-col m-10">
     <BarreDeRecherche  />
     <NuxtLink
       to="/meteo"
-      class="px-6 py-3 bg-blue-500 text-white font-bold rounded-full hover:bg-blue-700 transition duration-300 ease-in-out"
+      class="px-6 py-3 bg-blue-500 text-white font-bold rounded-full hover:bg-blue-700 transition duration-300 ease-in-out m-8"
     >
       Voir les infos météo
     </NuxtLink>
