da617a387492820c8045358f01726286a541071b
Lucas-Matusiak <49200401+Lucas-Matusiak@users.noreply.github.com>
2024-04-04 11:32:48 +0200

    ajout footer

--- app.vue
+++ app.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="">
+  <div class="flex justify-center items-center flex-col mx-10">
     <img
       src="~/../assets/images/logo.png"
       alt="Description de l'image"
-      class="flex justify-center w-20 h-auto mx-auto mt-8"
+      class="w-20 h-auto mx-4"
     />
     <NuxtPage />
+
+    <div class="flex items-center justify-center bg-white py-4">
+      <div class="flex space-x-4">
+        <button class="button">Nous contacter</button>
+        <button class="button">Mentions légales</button>
+        <button class="button">Nos réseaux</button>
+      </div>
+    </div>
   </div>
 </template>
 
-<!-- Pas besoin de définir des styles séparés avec Tailwind CSS -->
+<script setup>
+import { ref } from "vue";
+// Ajoutez ici vos variables réactives le cas échéant
+</script>
+
+<style scoped>
+/* Aucun style spécifique nécessaire, car Tailwind est utilisé directement dans les classes HTML */
+</style>
