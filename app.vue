3c81ed4a0fe08f99c93c53f318423f583d96b201
marega98 <66752505+marega98@users.noreply.github.com>
2024-04-08 16:59:54 +0200

    lever du soleil

--- app.vue
+++ app.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="h-screen bg-gradient-to-t from-indigo-400 to-sky-50">
-  <div class="flex justify-center items-center flex-col m-10 mt-0">
-    <img
-      src="~/../assets/images/logo.png"
-      alt="Description de l'image"
-      class="w-20 h-auto m-4 "
-    />
-    <NuxtPage />
+  <div class="min-h-screen bg-gradient-to-t from-indigo-400 to-sky-50">
+    <div class="flex justify-center items-center flex-col p-10">
+      <img
+        src="~/../assets/images/logo.png"
+        alt="Description de l'image"
+        class="w-20 h-auto m-4"
+      />
+      <NuxtPage />
 
-    <div class="flex items-center justify-center py-4 mt-0 ">
-      
-      <div class="flex space-x-4">
-        <button class="button">Nous contacter</button>
-        <button class="button">Mentions légales</button>
-        <button class="button">Nos réseaux</button>
+      <div class="flex items-center justify-center py-4 mt-10">
+        <div class="flex space-x-4">
+          <button class="button">Nous contacter</button>
+          <button class="button">Mentions légales</button>
+          <button class="button">Nos réseaux</button>
+        </div>
       </div>
-      
     </div>
   </div>
-</div>
 </template>
--- pages/meteo.vue
+++ pages/meteo.vue
@@ -13,6 +13,9 @@
       <div>
         <HumuditeVitesseDuVent/>
       </div>
+      <div>
+        <Soleil/>
+      </div>
 
     <!-- Bouton pour revenir à l'accueil -->
       <nuxt-link to="/" class="pl-20 pr-20 py-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">
