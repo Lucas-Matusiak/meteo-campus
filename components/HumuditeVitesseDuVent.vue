c0cee4d702a50287dabe7cb981c9d27bd4f76b8e
Lucas-Matusiak <lucassmatusiak@gmail.com>
2024-04-08 15:14:26 +0200

    fix composants + humidité + meteo

--- components/HumuditeVitesseDuVent.vue
+++ components/HumuditeVitesseDuVent.vue
@@ -1,36 +1,55 @@
 <template>
-    <div class= "flex pb-20">
-      <div class="pr-10 pt-5" >
-    <div class= "flex">
-      <div >
->>>>>>> c7ab811a6f8d840a9cf80abbd7016b38a148556e
-        <img src="~/../assets/images/Humidite.png" alt="Logo Humidité" class="logo" />
-        <h1 class ="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-white-000">{{ humidite }}%</h1>
-        <h1 class="text-1xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-white-000">Humidité</h1>
-      </div>
-<<<<<<< HEAD
-      <div class="wind-speed pl-4">
-=======
-      <div class="wind-speed">
->>>>>>> c7ab811a6f8d840a9cf80abbd7016b38a148556e
-        <img src="~/../assets/images/vent.png" alt="Logo Vitesse du vent" class="logo" />
-        <h1 class="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-white-000">{{ vitesseVent }} km/h</h1>
-        <h1 class="text-1xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-white-000">Vitesse du vent</h1>
+  <div class="flex pb-20">
+    <div class="pr-10 pt-5">
+      <div class="flex">
+        <div>
+          >>>>>>> c7ab811a6f8d840a9cf80abbd7016b38a148556e
+          <img
+            src="~/../assets/images/Humidite.png"
+            alt="Logo Humidité"
+            class="logo"
+          />
+          <h1
+            class="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-white-000"
+          >
+            {{ humidite }}%
+          </h1>
+          <h1
+            class="text-1xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-white-000"
+          >
+            Humidité
+          </h1>
+        </div>
+        <div class="wind-speed pl-4">
+          <img
+            src="~/../assets/images/vent.png"
+            alt="Logo Vitesse du vent"
+            class="logo"
+          />
+          <h1
+            class="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-white-000"
+          >
+            {{ vitesseVent }} km/h
+          </h1>
+          <h1
+            class="text-1xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-white-000"
+          >
+            Vitesse du vent
+          </h1>
+        </div>
       </div>
     </div>
-  </template>
-  
-  <script>
+  </div>
+</template>
 
+<script>
 export default {
-    name: 'HumiditeVitesseDuVent',
-    data() {
-      return {
-        humidite: 24,
-        vitesseVent: 22,
-      }
-    }
-  }
-  </script>
-  
- 
+  name: "HumiditeVitesseDuVent",
+  data() {
+    return {
+      humidite: 24,
+      vitesseVent: 22,
+    };
+  },
+};
+</script>
--- pages/meteo.vue
+++ pages/meteo.vue
@@ -3,7 +3,6 @@
   <div class="h-auto flex flex-col justify-center items-center">
     <h1 class="text-4xl font-bold text-center mb-4">Prévisions météo</h1>
     <div class=" "> <!-- Météo  -->
-<<<<<<< HEAD
       <div >
         <Temperature/>
       </div>
@@ -17,21 +16,5 @@
       </nuxt-link>
     </div>
 
-=======
-
-      <div class="flex justify-center mt-4">
-        <Temperature/>
-      </div>
-      <div> 
-        <HumuditeVitesseDuVent/>
-      </div>
-     
-    </div>
-  
-    <!-- Bouton pour revenir à l'accueil -->
-    <nuxt-link to="/" class="px-6 py-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">
-      Retour à l'accueil
-    </nuxt-link>
->>>>>>> c7ab811a6f8d840a9cf80abbd7016b38a148556e
   </div>
 </template>
