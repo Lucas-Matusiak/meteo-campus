fa85f323726cdc3429040334f50a7f48c6298cde
pierrelacome <102144251+pierrelacome@users.noreply.github.com>
2024-04-09 16:20:43 +0200

    création du composant AffichageHeure et ajout sur page meteo

---
+++ components/AffichageHeure.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        
+      <div class="container mx-auto">
+        <div
+        :class="{
+          'bg-blue-900':heure < 8,
+          'bg-gradient-to-tr from-blue-500 via-sky-700 to-blue-900': heure >= 8 && heure < 12,
+          'bg-gradient-to-br from-blue-500 via-sky-700 to-blue-900': heure >= 12 && heure < 17,
+          'bg-gradient-to-t from-blue-900 via-sky-700 to-blue-500': heure >= 17 && heure < 20,
+          'bg-blue-900': heure >= 20,
+        }"
+          class="w-20 h-40 rounded-lg border flex flex-col items-center justify-center m-1"
+        >
+          <div class="text-black text-center">
+            <p class="text_base">{{heure}} h</p>
+            <p class="text-base">{{temperature}}°</p>
+            <p class="text-base">{{ pourcentagePluie }}%</p>
+            <p class="text-base">{{vitesseVent}} km/h</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: "AffichageHeure",
+    props: ["heure","temperature","pourcentagePluie","vitesseVent"],
+  };
+  </script>
+  
--- pages/meteo.vue
+++ pages/meteo.vue
@@ -1,27 +1,109 @@
-
 <template>
   <div class="h-auto flex flex-col justify-center items-center">
     <h1 class="text-4xl font-bold text-center mb-4">Prévisions météo</h1>
-    <div class=" "> <!-- Météo  -->
+    <div class="flex flex-wrap justify-center">
+      <!-- Météo  -->
       <div>
-        <Temperature/>
-        <weather/>
+        <Temperature />
+        <weather />
+      </div>
+      <div></div>
+      <div>
+        <HumuditeVitesseDuVent />
       </div>
       <div>
-          
+        <Soleil />
       </div>
-      <div>
-        <HumuditeVitesseDuVent/>
-      </div>
-      <div>
-        <Soleil/>
-      </div>
-
-    <!-- Bouton pour revenir à l'accueil -->
-      <nuxt-link to="/" class="pl-20 pr-20 py-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">
-        Retour à l'accueil
-      </nuxt-link>
     </div>
-
+    <div class="overflow-x-auto max-w-full">
+      <div class="flex">
+        <AffichageHeure
+          v-for="(heure, index) in affichageheure"
+          :key="index"
+          :heure="heure.heure"
+          :temperature="heure.temperature"
+          :pourcentagePluie="heure.pourcentagePluie"
+          :vitesseVent="heure.vitesseVent"
+        />
+      </div>
+    </div>
+    <!-- Bouton pour revenir à l'accueil -->
+    <nuxt-link
+      to="/"
+      class="pl-20 pr-20 py-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out"
+    >
+      Retour à l'accueil
+    </nuxt-link>
   </div>
 </template>
+
+<script>
+import AffichageHeure from "~/components/AffichageHeure.vue";
+
+export default {
+  components: {
+    AffichageHeure,
+  },
+
+  data() {
+    return {
+      affichageheure: [
+        {
+          heure: "6",
+          temperature: "20",
+          pourcentagePluie: "76",
+          vitesseVent: "12",
+        },
+        {
+          heure: "9",
+          temperature: "20",
+          pourcentagePluie: "76",
+          vitesseVent: "12",
+        },
+        {
+          heure: "12",
+          temperature: "20",
+          pourcentagePluie: "76",
+          vitesseVent: "12",
+        },
+        {
+          heure: "15",
+          temperature: "20",
+          pourcentagePluie: "76",
+          vitesseVent: "12",
+        },
+        {
+          heure: "18",
+          temperature: "20",
+          pourcentagePluie: "76",
+          vitesseVent: "12",
+        },
+        {
+          heure: "21",
+          temperature: "20",
+          pourcentagePluie: "76",
+          vitesseVent: "12",
+        },
+        {
+          heure: "0",
+          temperature: "20",
+          pourcentagePluie: "76",
+          vitesseVent: "12",
+        },
+        {
+          heure: "12",
+          temperature: "20",
+          pourcentagePluie: "76",
+          vitesseVent: "12",
+        },
+        {
+          heure: "12",
+          temperature: "20",
+          pourcentagePluie: "76",
+          vitesseVent: "12",
+        },
+      ],
+    };
+  },
+};
+</script>
