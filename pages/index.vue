ef36234db7ab8f903d8370337c0a673dab531348
Lucas-Matusiak <lucassmatusiak@gmail.com>
2024-04-09 11:37:33 +0200

    recup info de barre de recherche  + fix css bouton meteo

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,11 @@
   <div class="m-8 flex justify-center items-center flex-col">
     <RechercheCampus @selected-campus="myCampus"/>
     <NuxtLink
-      to="/meteo",
-      params: { monParametre =
-      selectedCampus}
+      to="/meteo"
       class="px-6 py-3 bg-blue-500 text-white font-bold rounded-full hover:bg-blue-700 transition duration-300 ease-in-out"
     >
       Voir les infos météo
     </NuxtLink>
-    {{ selectedCampus }}
   </div>
 </template>
 
