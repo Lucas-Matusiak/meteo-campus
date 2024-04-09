9a7f1a04271795f22fc792bba304fa0b5f2baf94
marega98 <66752505+marega98@users.noreply.github.com>
2024-04-09 15:22:43 +0200

    le soleil est là en physique

---
+++ components/HumiditeVitesseDuVent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex pb-20 ml-5">
+         <div class = "pr-10 pt-4">
+             <img src="~/../assets/images/Humidite.png" alt="Logo Humidité" class="logo" />
+             <h1 class ="text-2xl font-bold bg-clip-text pt-5 text-center ">{{ humidite }}%</h1>
+             <h1 class="text-1xl font-bold bg-clip-text text-center ">Humidité</h1>
+         </div>
+
+         <div class="wind-speed pl-4">
+             <img src="~/../assets/images/vent.png" alt="Logo Vitesse du vent" class="logo" />
+             <h1 class="text-2xl font-bold bg-clip-text pt-5 text-center ">{{ vitesseVent }} km/h</h1>
+             <h1 class="text-1xl font-bold bg-clip-text text-center ">Vitesse du vent</h1>
+         </div>
+     </div>
+</template>
+<script>
+export default {
+name: "HumiditeVitesseDuVent",
+data() {
+return {
+ humidite: 24,
+ vitesseVent: 22,
+};
+},
+};
+</script>
