d2715a99020ff924b56d52d9fa4106ab58d6f4d6
Lucas-Matusiak <49200401+Lucas-Matusiak@users.noreply.github.com>
2024-04-10 15:09:53 +0200

    ajouter combobox test

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="m-8 flex justify-center items-center flex-col">
-    <RechercheCampus @selected-campus="setCampus($event)"/>
+    <!-- <RechercheCampus @selected-campus="setCampus($event)"/> -->
+    <Combobox/>
     <NuxtLink
       :to="`/meteo/${selectedCampus}`"
       class="px-6 py-3 bg-blue-500 text-white font-bold rounded-full hover:bg-blue-700 transition duration-300 ease-in-out"
@@ -11,7 +12,9 @@
 </template>
 
 <script setup>
+
 import RechercheCampus from "~/components/RechercheCampus.vue";
+import Combobox  from "~/components/Combobox.vue";
 const selectedCampus = ref(null);
 
 const setCampus = (value) => {
