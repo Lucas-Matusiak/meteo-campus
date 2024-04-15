b9da0872c53b3efe69a31cec4864fff55b89e4be
Lucas-Matusiak <lucassmatusiak@gmail.com>
2024-04-15 10:30:25 +0200

    fix appel composant avec val nul

--- pages/meteo.vue
+++ pages/meteo.vue
@@ -8,7 +8,7 @@
     <!-- Début de la zone du composant Temperature.vue -->
     <div class="items-center justify-center pb-5">
       <!-- Utilisation de v-bind pour passer weatherData à Temperature.vue -->
-      <Temperature :temperature="weatherData.current_weather.temperature" :feelsLike="weatherData.current_weather.feels_like_value" />
+      <Temperature v-if="weather" :temperature="weatherData.current_weather.temperature" :feelsLike="weatherData.current_weather.feels_like_value" />
     </div>
    
 
