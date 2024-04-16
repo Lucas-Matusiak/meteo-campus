15334ffba242ab7b33f5f8097dc505ca08808228
noan-miage <n.lecarboulec@gmail.com>
2024-04-16 16:53:01 +0200

    modifications des div : ajout d'un fond

--- components/HumiditeVitesseDuVent.vue
+++ components/HumiditeVitesseDuVent.vue
@@ -1,30 +1,32 @@
 <template>
-  <div class="flex justify-center">
-    <div class="pr-10 pt-4">
+  <div class="flex items-center justify-center">
+    <div class="pr-10 pt-4 bg-gradient-to-br from-[#469FBB] to-[#8BC5D6] rounded-3xl mb-4 shadow-lg mr-4">
       <div v-if="humidite !== null">
         <img
           src="~/../assets/images/Humidite.png"
           alt="Logo Humidité"
           class="logo"
+          style="width: 50px; height: auto"
         />
-        <h1 class="text-2xl font-bold bg-clip-text pt-5 text-center">
+        <h1 class="text-l font-bold bg-clip-text pt-5 text-center">
           {{ humidite }}%
         </h1>
-        <h1 class="text-1xl font-bold bg-clip-text text-center">Humidité</h1>
+        <h1 class="text-1xl bg-clip-text text-center">Humidité</h1>
       </div>
     </div>
 
-    <div class="wind-speed pl-4">
+    <div class="wind-speed pl-4 bg-gradient-to-br from-[#469FBB] to-[#8BC5D6] rounded-3xl mb-4 shadow-lg mr-4">
       <div v-if="vitesseVent !== null">
         <img
           src="~/../assets/images/vent.png"
           alt="Logo Vitesse du vent"
           class="logo"
+          style="width: 50px; height: auto"
         />
-        <h1 class="text-2xl font-bold bg-clip-text pt-5 text-center">
+        <h1 class="text-l font-bold bg-clip-text pt-5 text-center">
           {{ vitesseVent }} km/h
         </h1>
-        <h1 class="text-1xl font-bold bg-clip-text text-center">
+        <h1 class="text-1xl bg-clip-text text-center">
           Vitesse du vent
         </h1>
       </div>
--- components/Soleil.vue
+++ components/Soleil.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="flex flex-row items-center justify-around">
-      <div class="flex items-center">
+      <div class="flex items-center justify-center pr-10 pt-4 bg-gradient-to-br from-[#469FBB] to-[#8BC5D6] rounded-3xl mb-4 shadow-lg mr-4">
         <img src="~/../assets/images/lever-soleil.png" alt="Logo Lever du soleil" class=" w-10 " />
         <div class="ml-2">
-          <h1 class="text-lg font-bold">Lever</h1>
-          <h1 class="text-lg">{{ heureLever }}</h1>
+          <h1 class="text-l font-bold">Lever</h1>
+          <h1 class="text-l">{{ heureLever }}</h1>
         </div>
       </div>
-      <div class="flex items-center">
+      <div class="flex items-center justify-center pr-10 pt-4 bg-gradient-to-br from-[#469FBB] to-[#8BC5D6] rounded-3xl mb-4 shadow-lg mr-4">
         <img src="~/../assets/images/coucher-soleil.png" alt="Logo Coucher du soleil" class= "  w-10" />
         <div class="ml-2">
-          <h1 class="text-lg font-bold">Coucher</h1>
-          <h1 class="text-lg">{{ heureCoucher }}</h1>
+          <h1 class="text-l font-bold">Coucher</h1>
+          <h1 class="text-l">{{ heureCoucher }}</h1>
         </div>
       </div>
     </div>
