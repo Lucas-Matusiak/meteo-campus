57a268db7572a713b74774d3a1963c856a35c286
pierrelacome <102144251+pierrelacome@users.noreply.github.com>
2024-04-18 11:30:50 +0200

    création composant

---
+++ components/affichage-jours.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div
+      :class="[
+        'container',
+        { 'md:w-1/4': !isMobile }
+      ]"
+    >
+      <div
+        class="bg-gradient-to-b from-blue-400 to-blue-300 w-20 h-40 rounded-3xl flex flex-col items-center justify-center mt-4 mb-4 ml-2 mr-2 shadow-lg"
+      >
+        <div
+          class="text-black text_xs text-center flex flex-col items-center justify-center"
+        >
+          <!-- Affichage du jour de la semaine -->
+          <h1 class="text-xs text-white mt-2">{{ jourSemaine }}</h1>
+          <img
+            src="~/../assets/images/neige.png/"
+            alt=""
+            class="w-10 h-10 mt-2"
+          />
+          <p class="text-xs text-white m-2">{{ temperature }}°C</p>
+          <p class="text-xs text-white m-2">{{ min }}°C / {{ max }}°C</p>
+          <p class="text-xs text-white m-2">{{ vitesseVent }} km/h</p>
+          <img src="~/../assets/images/vent.png" alt="" class="w-4 h-4 mr-1" />
+          <p class="text-xs text-white m-2">Vitesse du vent</p>
+        </div>
+        <div class="hidden md:flex flex-row items-center justify-center m-1">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import { ref, computed } from "vue";
+
+const props = defineProps({
+  date: String,  // Assurez-vous que vous passez 'date' comme prop dans le composant parent
+  min: Number,
+  max: Number,
+  temperature: Number,
+  vitesseVent: Number,
+});
+
+const isMobile = computed(() => {
+  return window.innerWidth < 768;
+});
+
+// Convertir la date en nom de jour
+const jourSemaine = computed(() => {
+  const jours = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+  const dateObj = new Date(props.date);
+  return jours[dateObj.getDay()];
+});
+</script>
--- pages/meteo/[campus].vue
+++ pages/meteo/[campus].vue
@@ -1,69 +1,94 @@
 <template>
-  <div class="h-auto flex flex-col justify-center items-center w-full">
-    <h1 class="text-1xl font-bold text-center mb-2">
-      {{ route.params.campus }}
+  < class="h-auto flex flex-col justify-center items-center w-full">
+  <h1 class="text-1xl font-bold text-center mb-2">
+    {{ route.params.campus }}
+  </h1>
+  <div
+    v-if="weatherData && weatherData.current_weather"
+    class="items-center justify-center pb-5"
+  >
+    <Temperature
+      :temperature="weatherData.current_weather.temperature"
+      :feelsLike="weatherData.current_weather.feels_like_value"
+    />
+  </div>
+  <div
+    v-if="
+      weatherData &&
+      weatherData.current_weather &&
+      weatherData.current_weather.code
+    "
+  >
+    <weather :code="weatherData.current_weather.code" />
+  </div>
+  <div
+    v-if="
+      weatherData &&
+      weatherData.current_weather &&
+      weatherData.current_weather.humidity &&
+      weatherData.current_weather.wind_speed
+    "
+  >
+    <HumiditeVitesseDuVent
+      :humidite="weatherData.current_weather.humidity"
+      :vitesseVent="weatherData.current_weather.wind_speed"
+    />
+  </div>
+  <div v-if="weatherData && weatherData.current_weather">
+    <Soleil
+      :heureLever="weatherData.current_weather.sun_rise"
+      :heureCoucher="weatherData.current_weather.sun_set"
+    />
+  </div>
+
+  <div
+    class="bg-gradient-to-br from-[#469FBB] to-[#8BC5D6] rounded-3xl mb-4 shadow-lg w-[50%]"
+  >
+    <h1
+      class="text-center text-white font-bold border-b border-white px-4 m-4"
+      style="margin-top: 4px; margin-bottom: 4px"
+    >
+      Prévisions Heure par Heure
     </h1>
     <div
-      v-if="weatherData && weatherData.current_weather"
-      class="items-center justify-center pb-5"
+      class="flex overflow-x-scroll"
+      v-if="weatherData && weatherData.hourly_forecast"
     >
-      <Temperature
-        :temperature="weatherData.current_weather.temperature"
-        :feelsLike="weatherData.current_weather.feels_like_value"
+      <!-- Utilisez une boucle v-for pour afficher les données de prévisions horaires -->
+      <AffichageHeure
+        v-for="(data, index) in weatherData.hourly_forecast"
+        :key="index"
+        :heure="data.time"
+        :imgMeteo="data.weather_description"
+        :temperature="data.temperature"
+        :pourcentagePluie="data.precipitation_proba"
+        :vitesseVent="data.wind_speed"
       />
     </div>
-    <div
-      v-if="
-        weatherData &&
-        weatherData.current_weather &&
-        weatherData.current_weather.code
-      "
+  </div>
+  <div class="bg-gradient-to-br from-[#469FBB] to-[#8BC5D6] rounded-3xl mb-4 shadow-lg w-[50%]">
+    <h1
+      class="text-center text-white font-bold border-b border-white px-4 m-4"
+      style="margin-top: 4px; margin-bottom: 4px"
     >
-      <weather :code="weatherData.current_weather.code" />
-    </div>
-    <div
-      v-if="
-        weatherData &&
-        weatherData.current_weather &&
-        weatherData.current_weather.humidity &&
-        weatherData.current_weather.wind_speed
-      "
-    >
-      <HumiditeVitesseDuVent
-        :humidite="weatherData.current_weather.humidity"
-        :vitesseVent="weatherData.current_weather.wind_speed"
+      Prévisions de la semaine
+    </h1>
+    <div class="flex" v-if="weatherData && weatherData.hourly_forecast">
+      <!-- Utilisez une boucle v-for pour afficher les données de prévisions horaires -->
+      <AffichageJours
+        v-for="(data, index) in weatherData.daily_forecast"
+        :key="index"
+        :date="data.date"
+        :temperature="data.temperature_day"
+        :min="data.temperature_min"
+        :max="data.temperature_max"
+        :vitesseVent="data.wind_speed_kmh"
       />
     </div>
-    <div v-if="weatherData && weatherData.current_weather">
-      <Soleil
-        :heureLever="weatherData.current_weather.sun_rise"
-        :heureCoucher="weatherData.current_weather.sun_set"
-      />
-    </div>
+  </div>
 
-    <div
-      class="bg-gradient-to-br from-[#469FBB] to-[#8BC5D6] rounded-3xl mb-4 shadow-lg w-[50%]"
-    >
-      <h1
-        class="text-center text-white font-bold border-b border-white px-4 m-4"
-        style="margin-top: 4px; margin-bottom: 4px"
-      >
-        Prévisions Heure par Heure
-      </h1>
-      <div class="flex  overflow-x-scroll" v-if="weatherData && weatherData.hourly_forecast">
-        <!-- Utilisez une boucle v-for pour afficher les données de prévisions horaires -->
-        <AffichageHeure
-          v-for="(data, index) in weatherData.hourly_forecast"
-          :key="index"
-          :heure="data.time"
-          :imgMeteo="data.weather_description"
-          :temperature="data.temperature"
-          :pourcentagePluie="data.precipitation_proba"
-          :vitesseVent="data.wind_speed"
-        />
-      </div>
-    </div>
-    <!-- Bouton pour revenir à l'accueil -->
+  <div>
+   <!-- Bouton pour revenir à l'accueil -->
     <nuxt-link
       :to="`/universities/${selectedCampus}`"
       class="pl-20 pr-20 bg-[#469FBB] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out"
@@ -79,6 +104,7 @@ import axios from "axios";
 import AffichageHeure from "~/components/affichage-heure.vue";
 import Temperature from "~/components/temperature.vue";
 import HumiditeVitesseDuVent from "~/components/humidite-vitesse-vent.vue";
+import AffichageJours from "~/components/affichage-jours.vue";
 
 const route = useRoute();
 const selectedCampus = route.params.campus;
@@ -131,8 +157,5 @@ await api_call_localisation();
 if (lat.value && lon.value) {
   api_call_weather();
 }
-onMounted(() => {
-
-});
-
+onMounted(() => {});
 </script>
