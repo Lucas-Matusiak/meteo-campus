d6627ff0717927d48c7b3bd99492cc0317676cd9
Volucia <sandra5615@live.fr>
2024-04-18 10:43:37 +0200

    Erreur corrigé après le merge

--- back_api/api_restfull.py
+++ back_api/api_restfull.py
@@ -6,6 +6,7 @@ from urllib.parse import unquote
 from weather_api import get_current_weather, get_hourly_forecast, get_daily_forecast, weather_data_model
 from model_api import run_model
 
+#SI ERREUR VOICI LE COMMANDE : pip install scikit-learn==1.2.2 
 api_key = 'a1b1045de421855d4d44bb2b53d4da8f'
 
 app = Flask(__name__)
@@ -58,6 +59,151 @@ def complete_weather():
         "daily_forecast": daily_forecast_data,
         "model_response": model_response
     })
+
+
+@app.route('/api/universities')
+def get_universities():
+    db = os.getcwd() + '\\back_api\\campus.sqlite'
+    if os.path.exists(db):
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+
+        try:
+            # Retrieve data from SQLite database
+            cursor.execute("SELECT * FROM Universite")
+            universities = cursor.fetchall()
+
+            conn.close()
+
+            # Convert data to JSON format
+            university_list = []
+            for university in universities:
+                university_dict = {
+                    'id': university[0],
+                    'name': university[1],
+                    # Add other fields as needed
+                }
+                university_list.append(university_dict)
+
+            return jsonify(university_list)
+
+        except sqlite3.Error as e:
+            return jsonify({'error': str(e)}), 500
+        
+    else:
+        return jsonify({'error': "nul"}), 500
+
+@app.route('/api/implantations', methods=['GET'])
+def get_implantations():
+    db = os.getcwd() + '\\back_api\\campus.sqlite'
+    if os.path.exists(db):
+        try:
+            # Extract the etablissement_siege from the request query
+            etablissement_siege = request.args.get('etablissement_siege')
+
+            # Check if the etablissement_siege parameter is provided
+            if not etablissement_siege:
+                return jsonify({'error': 'Etablissement_siege parameter is missing'}), 400
+
+            
+            # Connect to SQLite database
+            conn = sqlite3.connect(db)
+            cursor = conn.cursor()
+
+            # Construct the SQL query to fetch the code_universite based on the provided etablissement_siege
+            query_get_code_universite = f"""
+                SELECT code_universite
+                FROM Universite
+                WHERE etablissement_siege = \'{etablissement_siege}\'
+            """
+
+            cursor.execute(query_get_code_universite)
+            universities = cursor.fetchall()
+
+            # Close database connection
+            conn.close()
+
+            code_universite = universities[0][0] if universities else None
+
+            # If code_universite is not found, return an empty array
+            if not code_universite:
+                return jsonify([])
+
+            # Connect to SQLite database again
+            conn = sqlite3.connect(db)
+            cursor = conn.cursor()
+
+            # Construct the SQL query to fetch implantations based on the code_universite
+            query_get_implantations = f"""
+                SELECT *
+                FROM Implantations
+                WHERE code_universite = '{code_universite}'
+            """
+
+            cursor.execute(query_get_implantations)
+            implantations = cursor.fetchall()
+
+            campus_list = []
+            for campus in implantations:
+                campus_dict = {
+                    'nom': campus[0],
+                    'latitude': campus[1],
+                    'longitude': campus[2],
+                    'id_campus': campus[3],
+                    'id_universite': campus[4],
+                }
+                campus_list.append(campus_dict)
+            
+            conn.close()
+
+            return jsonify(campus_list)
+
+        except Exception as e:
+            print(f"Error fetching implantations: {e}")
+            return jsonify({'error': 'Internal server error'}), 500
+
+@app.route('/api/campus_localisation', methods=['GET'])
+def get_campus_localisation():
+    db = os.getcwd() + '\\back_api\\campus.sqlite'
+    if os.path.exists(db):
+        try:
+            # Extract the etablissement_siege from the request query
+            campus = request.args.get('campus')
+            campus = unquote(campus)
+
+            # Check if the etablissement_siege parameter is provided
+            if not campus:
+                return jsonify({'error': 'Campus parameter is missing'}), 400
+
+            
+            # Connect to SQLite database
+            conn = sqlite3.connect(db)
+            cursor = conn.cursor()
+
+            query_get_localisation = f"SELECT latitude,longitude FROM Implantations WHERE nom_implantation=\'{campus}\'"
+
+
+            cursor.execute(query_get_localisation)
+            localisation_campus = cursor.fetchall()
+
+            campus_list = []
+            for campus in localisation_campus:
+                campus_dict = {
+                    'latitude': campus[0],
+                    'longitude': campus[1],
+                }
+                campus_list.append(campus_dict)
+
+            # Close database connection
+            conn.close()
+
+            return jsonify(campus_list)
+
+        except Exception as e:
+            # Handle any errors
+            print(f"Error fetching implantations: {e}")
+            return jsonify({'error': 'Internal server error'}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
