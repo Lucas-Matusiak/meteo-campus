71a8ca9db9b73a447d7e9b207ca53670a9257d06
pierrelacome <102144251+pierrelacome@users.noreply.github.com>
2024-04-19 11:31:38 +0200

    gestion image meteo

--- components/affichage-jours.vue
+++ components/affichage-jours.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="container mx-auto p-4">
-    <div class="bg-gradient-to-b from-blue-400 to-blue-300 rounded-3xl flex flex-col items-center justify-center shadow-lg m-2">
+  <div class="container mx-auto">
+    <div class="flex flex-col items-center justify-center">
       <!-- Contenu visible sur mobile uniquement -->
-      <div class="flex flex-row items-center justify-center text-center text-white p-4 md:hidden">
-        <img
-          src="~/../assets/images/neige.png"
-          alt="Neige"
-          class="w-14 h-14 mb-2"
-        />
-        <p class="text-lg">{{ temperature }}°C </p>
-        <h1 class="text-sm">{{ jourSemaine }} </h1>
+      <div
+        class="flex flex-row items-center justify-center text-center text-white border-t border-white px-4 mx-1 md:hidden"
+      >
+        <Weather :code="code" :showText="False" />
+        <p class="text-lg px-6">{{ temperature }}°C</p>
+        <h1 class="text-sm">{{ jourSemaine }}</h1>
       </div>
 
       <!-- Contenu visible sur tablette et desktop uniquement -->
-      <div class="hidden md:flex flex-col items-center justify-center text-center text-white p-4">
-        <h1 class="text-xs mt-2">{{ jourSemaine }} </h1>
-        <img
-          src="~/../assets/images/neige.png"
-          alt="Neige"
-          class="w-10 h-10 mt-2"
-        />
+      <div
+        class="hidden md:flex bg-gradient-to-b from-[#298DAD] to-[#A4D8E8] rounded-3xl flex-col items-center justify-center text-center text-white p-3 mb-4 mt-4"
+      >
+        <h1 class="text-xs mt-2">{{ jourSemaine }}</h1>
+        <Weather :code="code" :showText="False" />
         <p class="text-xs m-2">{{ temperature }}°C</p>
         <p class="text-xs m-2">{{ min }}°C / {{ max }}°C</p>
         <p class="text-xs m-2">{{ vitesseVent }} km/h</p>
-        <img src="~/../assets/images/vent.png" alt="Vent" class="w-4 h-4 mr-1" />
+        <img
+          src="~/../assets/images/vent.png"
+          alt="Vent"
+          class="w-4 h-4 mr-1"
+        />
         <p class="text-xs m-2">Vitesse du vent</p>
       </div>
     </div>
@@ -32,6 +32,7 @@
 
 <script setup>
 import { ref, computed } from "vue";
+import Weather from "./weather.vue";
 
 const props = defineProps({
   date: String,
@@ -39,11 +40,20 @@ const props = defineProps({
   max: Number,
   temperature: Number,
   vitesseVent: Number,
+  code: String,
 });
 
 // On utilise directement les classes de Tailwind pour la réactivité donc pas besoin de computed isMobile.
 const jourSemaine = computed(() => {
-  const jours = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+  const jours = [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+  ];
   const dateObj = new Date(props.date);
   return jours[dateObj.getDay()];
 });
---
+++ components/ressenti.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="items-center justify-center">
+
+    <div v-if="temperature !== null && feelsLike !== null">
+      <p class="text-center text-2xl bg-clip-text text-transparent bg-gradient-to-l from-black to-black">{{ temperature }}°C</p>
+      <p class="text-center text-1xl bg-clip-text text-transparent bg-gradient-to-b from-black to-black">Ressenti {{ feelsLike }}°C</p>
+    </div>
+   
+    <div v-else>
+      <p>Loading...</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  // Déclaration des variables et importation de defineProps
+  import { defineProps } from 'vue';
+
+  // Définition des propriétés du composant
+  const props = defineProps({
+    temperature: { type: Number, default: null },
+    feelsLike: { type: Number, default: null }
+  });
+</script>
+
+<style scoped>
+/* Styles CSS spécifiques au composant ici */
+</style>
