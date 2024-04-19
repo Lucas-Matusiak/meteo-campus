3186805ae1870ab471858facc9482c0e71161719
Lucas-Matusiak <lucassmatusiak@gmail.com>
2024-04-19 17:33:22 +0200

    grossissement titre température

--- components/temperature.vue
+++ components/temperature.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col mb-12">
 
     <div v-if="temperature !== null && feelsLike !== null">
-      <p class="text-6xl bg-clip-text text-white font-bold py-2 ">{{ temperature }}°C</p>
+      <p class="text-7xl bg-clip-text text-white font-bold py-3">{{ temperature }}°C</p>
       <p class="text-sm bg-clip-text text-white">Ressenti
         <span class="text-center text-1xl bg-clip-text text-white font-bold"> {{ feelsLike }}°C</span>
       </p> 
