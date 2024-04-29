13868342410d46198f830431e3a03067c12af9a2
Lucas-Matusiak <lucassmatusiak@gmail.com>
2024-04-29 10:14:04 +0200

    suppression prédiction

--- components/accessoire-tenue.vue
+++ components/accessoire-tenue.vue
@@ -1,10 +1,8 @@
 <template>
   <h1 class="text-1xl font-bold text-center">StylAI</h1>
-
   <div class="flex justify-center">
-    <!-- Bloc pour l'accessoire -->
     <div
-      v-if="accessoireprediction !== null"
+      v-if="accessoire !== null"
       class="flex items-center pr-1 pt-4 "
     >
       <div class="flex flex-col items-center ">
@@ -13,23 +11,15 @@
           alt="Logo Accessoire"
           class="w-20 h-auto mb-2"
         />
-        <h1 class="text-1xl font-bold bg-clip-text pt-1 text-center">
-          Prédit à {{ accessoireprediction }}%
-        </h1>
       </div>
     </div>
-
-    <!-- Bloc pour la tenue -->
-    <div v-if="tenueprediction !== null" class="flex items-center pt-4">
+    <div v-if="tenue !== null" class="flex items-center pt-4">
       <div class="flex flex-col items-center">
         <img
           src="~/../assets/images/Tenue0.svg"
           alt="Logo Tenue"
           class="w-20 h-auto mb-2"
         />
-        <h1 class="text-1xl font-bold bg-clip-text pt-1 text-center">
-          Prédit à {{ tenautoueprediction }}%
-        </h1>
       </div>
     </div>
   </div>
